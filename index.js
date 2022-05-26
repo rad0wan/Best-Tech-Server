c810a304c2ef79d5045a8cf732d83d83c8f1c1e0
Ridwan <radowan.me@gmail.com>
2022-05-26 09:16:44 +0600

    review api post method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -16,10 +16,30 @@ async function run() {
     try {
         await client.connect()
         const productCollection = client.db('best-tech0').collection('product')
+        const reviewCollection = client.db('best-tech0').collection('review')
 
         app.get('/product', async (req, res) => {
             const products = await productCollection.find().toArray();
             res.send(products);
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            let id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            console.log(query);
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
+
+        app.get('/review', async (req, res) => {
+            const reviews = await reviewCollection.find().toArray();
+            res.send(reviews);
+        })
+
+        app.post('/review', async (req, res) => {
+            const doctor = req.body;
+            const result = await reviewCollection.insertOne(doctor)
+            res.send(result)
         })
     }
     finally {
