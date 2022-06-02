bfe4a8b0d77c48e674ca8eb00997e1f22828d394
Ridwan <radowan.me@gmail.com>
2022-06-02 07:38:00 +0600

    msg

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors())
 app.use(express.json())
-app.use(cors({ origin: 'https://best-tech0.web.app/' }))
+// app.use(cors({ origin: 'https://best-tech0.web.app/' }))
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@best-tech0.ucbvt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
